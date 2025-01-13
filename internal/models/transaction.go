754384c5dc6416d1b65ffb76c5836904db92cdaf
nivschuman <niv.schuman@gmail.com>
2025-01-13 23:23:51 +0200

    wallet and fixed transaction

--- internal/models/transaction.go
+++ internal/models/transaction.go
@@ -1,36 +1,32 @@
 package models
 
 import (
-	"fmt"
+	"encoding/binary"
 
 	"github.com/nivschuman/VotingBlockchain/internal/crypto/hash"
-	"github.com/nivschuman/VotingBlockchain/internal/crypto/ppk"
 )
 
 type Transaction struct {
-	Id             []byte //hash of (CandidateId, Year, VoterPublicKey), 32 bytes
-	CandidateId    uint32 //id of candidate to vote for, 4 bytes
-	Year           uint32 //year of vote, 4 bytes
-	VoterPublicKey []byte //marshal compressed ECDSA public key, 33 bytes
-	Signature      []byte //signature of Id, 64 bytes, in ASN1 format, 70-72 bytes
+	Id          []byte //hash of (CandidateId, Year, Term, WalletId), 32 bytes
+	CandidateId uint32 //id of candidate to vote for, 4 bytes
+	Year        uint32 //year of vote, 4 bytes
+	Term        uint32 //term of vote in year, 4 bytes
+	WalletId    []byte //wallet id, 32 bytes (hash)
+	Signature   []byte //signature of Id, 64 bytes, in ASN1 format, 70-72 bytes
 }
 
 func (transaction *Transaction) GetTransactionHash() []byte {
-	data := fmt.Sprintf("%d%d%s", transaction.CandidateId, transaction.Year, transaction.VoterPublicKey)
-	return hash.HashStringAsBytes(data)
+	buf_size := 4 + 4 + 4 + len(transaction.WalletId)
+	buf := make([]byte, buf_size)
+
+	binary.BigEndian.PutUint32(buf[0:4], transaction.CandidateId)
+	binary.BigEndian.PutUint32(buf[4:8], transaction.Year)
+	binary.BigEndian.PutUint32(buf[8:12], transaction.Term)
+	copy(buf[12:], transaction.WalletId)
+
+	return hash.HashBytes(buf)
 }
 
 func (transaction *Transaction) SetId() {
 	transaction.Id = transaction.GetTransactionHash()
-}
-
-func (transaction *Transaction) IsValidSignature() (bool, error) {
-	hash := transaction.GetTransactionHash()
-	publicKey, err := ppk.GetPublicKeyFromBytes(transaction.VoterPublicKey)
-
-	if err != nil {
-		return false, err
-	}
-
-	return publicKey.VerifySignature(transaction.Signature, hash), nil
 }
